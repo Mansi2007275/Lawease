6662747baa0db534c5de80ec05171ba26fb164dd
Mansi yadav <yadavmansi0111207@gmail.com>
2025-10-19 19:47:02 +0530

    Added footer component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import Footer from "./components/Footer.jsx";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
